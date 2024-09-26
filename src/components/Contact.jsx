4b25ffaef4ed857218a2d2dd52c838888b9e96ca
dineshdevelope <dineshindevelope@gmail.com>
2024-09-27 00:44:31 +0530

    edit:form-section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
         <div className="max-w-[800px] mx-auto">
           <div className="mt-6 bg-[#161616] rounded-xl">
             <div className="p-10">
-              <form action="#" method="POST">
+              <form action="https://getform.io/f/bdryjyxb" method="POST">
                 <div className="grid grid-cols-1 sm:grid-cols-2 gap-x-5 gap-y-4 ">
                   <div>
                     <div className="mt-2.5 ">
